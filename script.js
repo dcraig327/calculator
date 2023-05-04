c038e725f6b70353336da5ac77f5ca68183b3538
David Craig <37514553+dcraig327@users.noreply.github.com>
2023-05-04 00:15:45 -0700

    render pressed number button in display

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
-function add(a,b) {
-  return a+b;
-}
-function subtract(a,b) {
-  return a-b;
-}
-function multiply(a,b) {
-  return a*b;
-}
-function divide(a,b) {
-  return a/b;
-}
+let display;
 
-
-
-//first number, the operator, and the second number
-let inputFirstNumber = "";
-let inputOperator = "";
-let inputSecondNumber = "";
+////////////////////////////////////////////////////////////////////////////////
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
 
 function operate(a, operator, b) {
-  switch(operator) {
-    case '+':
-      return add(a,b);
-      break;
-    case '-':
-      return subtract(a,b);
-      break;
-    case '*':
-      return multiply(a,b);
-      break;
-    case '/':
-      return divide(a,b);
-      break;
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
     default:
       console.log("Not a valid operator.");
       break;
   }
 }
+
+function buttonPress() {
+  switch (this.textContent) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      display.textContent = this.textContent;
+      break;
+    default:
+      break;
+  }
+}
+
+function init() {
+  display = document.querySelector(".display");
+
+  let buttons = document.querySelectorAll("button");
+  buttons.forEach(button => {
+    button.addEventListener("click", buttonPress);
+  });
+}
+
+////////////////////////////////////////////////////////////////////////////////
+init();
